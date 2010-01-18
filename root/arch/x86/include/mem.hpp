9f8616af1f1da7c5c48f222c3bd2dbbb5dad600c
System User <take@pear.katox.dip.jp>
2010-01-18 10:54:29 +0900

    2010 start.

---
+++ root/arch/x86/include/mem.hpp
@@ -0,0 +1,18 @@
+/* @file    arch/x86/include/mem.hpp
+ * @version 0.0.1
+ * @date    2009-08-09
+ * @author  Kato Takeshi
+ * @brief   x86のメモリ周りの定義。
+ */
+// (C) Kato Takeshi 2009
+
+#ifndef _ARCH_X86_INCLUDE_MEM_HPP
+#define _ARCH_X86_INCLUDE_MEM_HPP
+
+
+#define PAGE_SIZE_SHIFT  12
+
+
+
+
+#endif // _ARCH_X86_INCLUDE_MEM_HPP
