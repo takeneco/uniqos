edef2a583ecc8c8cea3d929a5154d93262242559
System User <take@pear.katox.dip.jp>
2010-08-05 13:17:22 +0900

    2010-08-03 deep night end

--- root/arch/x86_64/kernel/physical_memmgr.cc
+++ root/arch/x86_64/kernel/physical_memmgr.cc
@@ -16,11 +16,20 @@ namespace
 /// 1bitで空き状態を記憶する。
 class physical_4kmemblk_cluster
 {
+	static u64 cluster_base_addr;
+
 	/// 64 * 2 blocks
 	u64 free_mem_bitmap[2];
 public:
 	bichain_link<physical_4kmemblk_cluster> chain_item;
+
+public:
+	static void set_base_addr(u64 base) {
+		cluster_base_addr = base;
+	}
+	u64 get_base_addr();
 };
+u64 physical_4kmemblk_cluster::cluster_base_addr;
 
 typedef
     bichain<physical_4kmemblk_cluster, &physical_4kmemblk_cluster::chain_item>
@@ -29,20 +38,83 @@ typedef
 pmem_cluster_chain free_chain;
 pmem_cluster_chain used_chain;
 
-u64 get_physical_memory_end()
+inline u64 physical_4kmemblk_cluster::get_base_addr()
 {
-	setup_memmgr_dumpdata* memmap;
-	u32 memmap_num;
-	setup_get_free_memmap(&memmap, &memmap_num);
+	return reinterpret_cast<u64>(this) - cluster_base_addr;
+}
 
-	// ここで memmap の最後の方から物理メモリの終端アドレスを計算する。
+/// 物理メモリサイズをバイト数で返す。
+/// セットアップ後の空きメモリのうち、
+/// 最大のアドレスを物理メモリの終端アドレスとして計算する。
+u64 get_physical_memory_size()
+{
+	setup_memmgr_dumpdata* freemap;
+	u32 freemap_num;
+	setup_get_free_memmap(&freemap, &freemap_num);
+
+	u64 total_end = 0L;
+	for (u32 i = 0; i < freemap_num; i++) {
+		const u64 end = freemap[i].head + freemap[i].bytes;
+		if (end > total_end)
+			total_end = end;
+	}
+
+	return total_end;
+}
+
+/// セットアップ後の空きメモリ情報から、
+/// 連続した物理メモリの空き領域を探す。
+/// @param[in] size 必要な最小のメモリサイズ。
+/// @return 適当に見つけた setup_memmgr_dumpdata のポインタを返す。
+/// @return 無い場合は nullptr を返す。
+setup_memmgr_dumpdata* search_seriesof_free_physical_memory(u64 size)
+{
+	setup_memmgr_dumpdata* freemap;
+	u32 freemap_num;
+	setup_get_free_memmap(&freemap, &freemap_num);
+
+	for (u32 i = 0; i < freemap_num; i++) {
+		if (freemap[i].bytes >= size)
+			return &freemap[i];
+	}
+
+	return 0;
 }
 
 } // namespace
 
+/// @retval cause::NO_MEMORY  No enough physical memory.
+/// @retval cause::OK  Succeeds.
 cause::stype phymemmgr_init()
 {
-	
+	const u64 phymem_size = get_physical_memory_size();
+
+	setup_memmgr_dumpdata* phymem_data =
+	    search_seriesof_free_physical_memory(phymem_size);
+	if (phymem_data == 0)
+		return cause::NO_MEMORY;
+
+	// 物理メモリのページ数
+	const u64 phymem_pages = phymem_size / 0x1000;
+	// すべての物理メモリページを管理するために必要な cluster 数
+	const u64 cluster_num = (phymem_pages + 127) / 128;
+	// cluster が使用するメモリサイズ
+	const u64 phymemmgr_buf_size =
+	    cluster_num * sizeof (physical_4kmemblk_cluster);
+
+	const u64 cluster_base_addr = phymem_data->head;
+	phymem_data->head += phymemmgr_buf_size;
+	phymem_data->bytes -= phymemmgr_buf_size;
+
+	physical_4kmemblk_cluster* cluster =
+	    reinterpret_cast<physical_4kmemblk_cluster>
+	    (cluster_base_addr + 0xffff800000000000);
+
+	physical_4kmemblk_cluster::set_base_addr(cluster_base_addr);
+
+
+
+	return cause::OK;
 }
 
 /// 空きメモリをメモリ管理に取り込む。
