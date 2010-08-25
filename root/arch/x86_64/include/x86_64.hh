0a2bd7711437e68b3a804e9c289369ab6b894a99
Takeshi KATO <take@pear.katox.dip.jp>
2010-08-26 01:54:46 +0900

    2010-08-25 deep night end COMの有無をテストできるか調べたい。

---
+++ root/arch/x86_64/include/x86_64.hh
@@ -0,0 +1,18 @@
+/// @author Kato Takeshi
+/// @brief  x86_64 hardware parameters.
+///
+/// (C) 2010 Kato Takeshi
+
+#ifndef _ARCH_X86_64_INCLUDE_X86_64_HH_
+#define _ARCH_X86_64_INCLUDE_X86_64_HH_
+
+#include "btypes.hh"
+
+
+enum {
+	IRQ_CPU_OFFSET = 0x00,
+	IRQ_PIC_OFFSET = 0x20,
+};
+
+
+#endif  // Include guard.
