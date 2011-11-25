7fcbc398eeb768c33223a016661a61e8bbdfd741
KATO Takeshi <takeneco@users.sourceforge.jp>
2011-11-25 18:38:21 +0900

    preload.cc fixed

--- root/arch/x86_64/boot/multiboot/preload.cc
+++ root/arch/x86_64/boot/multiboot/preload.cc
@@ -1,7 +1,20 @@
 /// @file   preload.cc
+
+//  uniqos  --  Unique Operating System
+//  (C) 2011 KATO Takeshi
 //
-// (C) 2011 KATO Takeshi
+//  uniqos is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //
+//  uniqos is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "bootinfo.hh"
 #include "log.hh"
