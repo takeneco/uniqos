c6a217077c08184469be454f77bd2e17cf9195c9
KATO Takeshi <takeneco@users.sourceforge.jp>
2013-08-11 10:36:18 +0900

    thread_ctl.hh added

---
+++ arch/x86_64/include/arch/thread_ctl.hh
@@ -0,0 +1,18 @@
+/// @file  thread.hh
+//
+// (C) 2013 KATO Takeshi
+//
+
+#ifndef ARCH_X86_64_INCLUDE_ARCH_THREAD_HH_
+#define ARCH_X86_64_INCLUDE_ARCH_THREAD_HH_
+
+
+namespace arch {
+
+void sleep_current_thread();
+
+}  // namespace arch
+
+
+#endif  // include guard
+
