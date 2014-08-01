afc0e040c07098d9f5ad81b775c267f2ea15f79b
KATO Takeshi <takeneco@users.sourceforge.jp>
2014-08-02 04:07:36 +0900

    mount syscall

--- arch/x86_64/source/first/main.cc
+++ arch/x86_64/source/first/main.cc
@@ -1,9 +1,27 @@
 /// @file  main.cc
 /// @brief first process main function.
 
+typedef unsigned long uptr;
+
+struct syscall_r
+{
+	uptr ret;
+	uptr data;
+};
+
+extern "C" syscall_r syscall5(uptr, uptr, uptr, uptr, uptr, uptr);
+
+syscall_r mount(const char* source, const char* target, const char* type,
+unsigned long mountflags, const void* data)
+{
+	return syscall5((uptr)source, (uptr)target, (uptr)type,
+	    mountflags, (uptr)data, 101);
+}
+
 
 extern "C" int main()
 {
+	mount(0, "/", "ramfs", 0, 0);
 	for (;;) {
 		asm volatile ("syscall" : : "a"(100));
 	}
--- arch/x86_64/source/syscall_entry.cc
+++ arch/x86_64/source/syscall_entry.cc
@@ -22,8 +22,27 @@
 #include <core/timer.hh>
 
 
+cause::pair<uptr> test_string(const char* str)
+{
+	uptr i;
+	for (i = 0; str[i]; ++i)
+		;
+
+	return make_pair(cause::OK, i);
+}
+
 extern "C" cause::pair<uptr> syscall_entry(const ucpu* data)
 {
+	/* data[0] : %rax
+	 * data[1] : %rip
+	 * data[2] : %rdx(3rd param)
+	 * data[3] : %rsi(2nd param)
+	 * data[4] : %rdi(1st param)
+	 * data[5] : %r8 (5th param)
+	 * data[6] : %r9 (6th param)
+	 * data[7] : %r10(4th param)
+	 * data[8] : %eflags
+	 */
 	log()('#');
 
 	if (data[0] == 100) { // timer
@@ -34,6 +53,17 @@ extern "C" cause::pair<uptr> syscall_entry(const ucpu* data)
 		wttm.nanosec_delay = 100000000;
 		timer_set(&wttm);
 		sleep_current_thread();
+	}
+	else if (data[0] == 101) { // mount
+		const char* _src = (const char*)data[4];
+		const char* _tgt = (const char*)data[3];
+		const char* _typ = (const char*)data[2];
+		u32 _flg = (u32)data[7];
+		const void* _data = (const void*)data[5];
+		uptr val = (uptr)data[6];
+
+		log()("mount():src=")(_src)(", tgt=")(_tgt)(", typ=")(_typ)
+		    (", flg=").x(_flg)(", data=").p(_data)()(", 6=").x(val)();
 	}
 
 	return cause::pair<uptr>(cause::OK, 0);
